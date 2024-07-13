9dee443880be8ca50165707723d9bb9c0a655601
dacarlson6 <dacarlson6@wisc.edu>
2024-07-13 09:22:37 -0500

    comment for peer code review assignment

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,15 @@ import React from 'react'
 import ScrollArrow from './ScrollArrow'
 import '../App.css'
 
+//Comment from Devin Carlson for the Midterm Peer Review assignment 7/13/24
+//Wow!!! This is amazing!  First, well done on this site.  Love the idea and I love the branding.
+//I am not a natural coder and rely heavily on course material, so your structure of your project gave me a view into an organized and robust program
+//Truthfully, I had a hard time finding something to critique/improve/change.
+//I shared this with my 15 year old son to ask him to help me find something that could be improved.
+//his only response was that he could have used more context as to what your goals are for this
+//and what this could be used for.
+//Overall, excellent job and thanks for sharing!!!
+
 const AboutSection = () => {
   return (
     <div className="background-container">
